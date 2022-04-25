77eee1c4edc2de91af7c68683e83bf9592473bb5
rishikrishna <r.rishikrishna@gmail.com>
2022-04-25 15:41:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Notes App
+
+## Technologies Used: 
+- Markdown -v4
+- In built Local Storage
+
+<a href="https://rishi1011.github.io/notes-app/">Live Demo</a>
---
+++ script.js
@@ -0,0 +1,77 @@
+const addBtn = document.getElementById('add');
+
+const notesWithin = JSON.parse(localStorage.getItem('notes'));
+
+if (notesWithin != null)
+    notesWithin.forEach(noteValue => {
+        addNewNote(noteValue);
+    });
+
+addBtn.addEventListener('click',() =>  addNewNote());
+
+function addNewNote(text = '') {
+    const note = document.createElement('div');
+    note.classList.add('note');
+
+    console.log(text === '');
+
+    note.innerHTML = `
+        <div class="tools">
+            <button class="edit" id="edit"><i class="fa-solid fa-pen-to-square"></i></button>
+            <button class="check hidden" id="check"><i class="fa-solid fa-check"></i></button>
+            <button class="delete" id="delete"><i class="fa-solid fa-trash-can"></i></button>
+        </div>
+        <div class="main ${text === '' ? 'hidden' : ''} "></div>
+        <textarea class="${text === '' ? '' : 'hidden'} "></textarea>
+    `;
+
+    document.body.appendChild(note);
+    window.scrollTo(0, note.offsetTop);
+
+    const editBtn = note.querySelector('.edit');
+    const deleteBtn = note.querySelector('.delete');
+    const checkBtn = note.querySelector('.check');
+    const textarea = note.querySelector('textarea');
+    const main = note.querySelector('.main');
+
+    main.innerHTML = marked.parse(text);
+    textarea.value = text;
+
+    editBtn.addEventListener('click', () => {
+        toggleTextDisplay(textarea, main);
+        textarea.focus();
+        checkBtn.classList.remove('hidden');
+    });
+
+    textarea.addEventListener('click', () => checkBtn.classList.remove('hidden'));
+
+    checkBtn.addEventListener('click', () => {
+        toggleTextDisplay(textarea, main);
+        const value = textarea.value;
+        main.innerHTML = marked.parse(value);
+        checkBtn.classList.add('hidden');
+        updateLS();
+    });
+
+    deleteBtn.addEventListener('click', () => {
+        note.remove();
+        updateLS();
+    });
+}
+
+function toggleTextDisplay(textarea, main) {
+    textarea.classList.toggle('hidden');
+    main.classList.toggle('hidden');
+}
+
+function updateLS() {
+    const notes = document.querySelectorAll('textarea');
+
+    let notesArray = []
+
+    notes.forEach(note => {
+        notesArray.push(note.value);
+    });
+
+    localStorage.setItem('notes', JSON.stringify(notesArray));
+}
